ce4143aa61482795107969d9e760c3c480fb86ca
wuwenqing <wuwenqing@aliyun.com>
2018-03-31 18:51:17 +0800

    first commit

---
+++ gcs-sdk/src/main/java/com/gcs/sdk/App.java
@@ -0,0 +1,13 @@
+package com.gcs.sdk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
